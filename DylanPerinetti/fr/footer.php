a5f860d328c880a8cb1b9e3951891579048c550e
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-28 19:46:33 +0200

    Add files via upload

---
+++ DylanPerinetti/fr/footer.php
@@ -0,0 +1,20 @@
+<footer>
+	<ul class="footer">
+		<li>
+			<a title="Consulter mes codes sur GitHub" href="https://github.com/dylanPerinetti">
+				<img src="../image/github.png" height="30px">
+			</a>
+		</li>
+		<li>
+			<a title="Consulter ma chaîne Youtube" href="https://www.youtube.com/channel/UCnuvdyzzjYsLgJLK4OgsYAQ/featured">
+				<img src="../image/youtube.png" width="30px">
+			</a>
+		</li>
+		<li>
+			<a title="Lien vers mon compte instagram" href="">
+				<img src="../image/instagram.png" width="30px">
+			</a>
+		</li>
+		<li><p>Copyright ©2022 DYLANPERINETTI</p></li>
+	</ul>
+</footer>
---
+++ DylanPerinetti/fr/nav.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<div class="item it-nav">
+		<a href="index.php">
+			<img class="logo" src="../image/logo1.png" height="200px" >
+		</a>
+		<ul>
+			<li>
+				<a href="index.php">>_ Acceuil</a>
+			</li>
+			<li>
+				<a href="FAQ.php">>_ F.A.Q</a>
+			</li>
+			<li>
+				<a href="link.php">>_ Liens</a>
+			</li>
+			<!--<li>
+				<a href="">>_ Bio</a>
+			</li>-->
+			<li>
+				<a href="contact.php">>_ Contact</a>
+			</li>
+		</ul>
+	</div>
+	<div class="item it-titre">
+		<h1><?= $title ?></h1>
+	</div>
