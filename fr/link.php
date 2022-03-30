a1eef7aa9418e7578bee23fae6e6a4d6d0954f42
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2022-03-30 22:52:26 +0200

    Update link.php

--- fr/link.php
+++ fr/link.php
@@ -8,15 +8,19 @@ require'nav.php';
 		<h2>>_ Permier fois #1</h2>
 		<li>
 			<a class="link" href="https://github.com/OpenRTVC/openrtvc" title="Lien vers le Git de mon projet fusée">• OpenRTVC (Git): </a>
-			<p>Il s'agit de mon premier projet de fusée dirigé par un TVC (Trust Vector Controler)</p>
+			<p>Il s'agit de mon premier projet de fusée dirigée par un TVC (Trust Vector Controler)</p>
 		</li>
 		<li>
 			<a class="link" href="https://openrtvc.tech" title="Lien vers mon site heberger">• OpenRTVC.tech : </a>
-			<p>Il s'agit de mon premier Site web (Hébergé chez <a href="https://ovh.com" title="Lien vers le site OVH">OVH</a>).Qui porte sur le projet ci-dessus</p>
+			<p>Il s'agit de mon premier site web (Hébergé chez <a href="https://ovh.com" title="Lien vers le site OVH">OVH</a>) qui porte sur le projet ci-dessus</p>
 		</li>
 		<li>
 			<a class="link" href="https://TheVentureThermos.fr" title="Lien vers ma boutique de e-comerce">• TheVentureThermos.fr : </a>
-			<p>C'est ma premer boutique de e-commerce.</p>
+			<p>C'est ma premiére boutique de e-commerce.</p>
+		</li>
+		<li>
+			<a class="link" href="https://www.youtube.com/channel/UCMrQVNrV_wyv7dx9DSc1_rQ" title="Lien vers ma première chaîne youtube">• TheLinuxProject (YouTube): </a>
+			<p>C'est ma premiére chaîne sur youtube.</p>
 		</li>
 	</ul>
 </div>
